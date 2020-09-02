909ad3fdb61f5aeb554a6ac77e8ccda87261e0f2
TatyCoder <tmilanes@gmail.com>
2020-09-02 15:12:16 -0400

    Practice OOP

---
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,7 @@
+class Tooltip {}  // Which will be helpful for the More Info button.
+
+class ProjectItem {}  // Which will be helpful for listening on clicks on the Finish or Activate button.
+
+class ProjectList {}  // Which manages all the projects of one box. We can create multiple instances of this class for the different lists we have.
+
+class App {}  // Which manages the app overall.
---
+++ practice-oop-02-created-lists-basic-logic/assets/scripts/app.js
@@ -0,0 +1,20 @@
+class Tooltip {}
+
+class ProjectItem {}
+
+class ProjectList {
+  constructor(type) {  // type for choosing between 'active or 'finished' projects (id).
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);  
+    // CSS selector for all list items with these IDs.
+    console.log(prjItems);
+  }
+}
+
+class App {
+  static init() {  // A static method because I'll call it just once with App.init();
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+  }
+}
+
+App.init();
