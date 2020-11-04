6448f736928b48bf5324678aa9cc406bc97196fd
TatyCoder <tmilanes@gmail.com>
2020-11-04 16:26:31 -0500

    Http Requests Exercises

---
+++ http-09-headers/assets/scripts/app.js
@@ -0,0 +1,100 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
+
+function sendHttpRequest(method, url, data) {
+  // const promise = new Promise((resolve, reject) => {
+  // const xhr = new XMLHttpRequest();
+  // Another way to set headers:   
+  // xhr.setRequestHeader('Content-Type', 'application/json');
+  // The 1st argument is the header name, and the 2nd argument is the value for that header. 
+
+  //   xhr.open(method, url);
+
+  //   xhr.responseType = 'json';
+
+  //   xhr.onload = function() {
+  //     if (xhr.status >= 200 && xhr.status < 300) {
+  //       resolve(xhr.response);
+  //     } else {
+  //       reject(new Error('Something went wrong!'));
+  //     }
+  //     // const listOfPosts = JSON.parse(xhr.response);
+  //   };
+
+  //   xhr.onerror = function() {
+  //     reject(new Error('Failed to send request!'));
+  //   };
+
+  //   xhr.send(JSON.stringify(data));
+  // });
+
+  // return promise;
+
+  // Changed, adding headers, which are metadata I can attach to outgoing requests:
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json' 
+    }
+    /* I can add headers to the outgoing request on fetch by adding a new key to that 
+    configuration object which is the headers key. This takes a Javascript object where 
+    every header which I want to add is a key-value pair.
+    Above is the typical header to outgoing requests which carry JSON data.
+    */
+  }).then(response => {
+    return response.json();
+  });
+}
+
+async function fetchPosts() {
+  // try {
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    postEl.querySelector('li').id = post.id;
+    listElement.append(postEl);
+  }
+  // } catch (error) {
+  //   alert(error.message);
+  // }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId
+  };
+
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+}
+
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector('#title').value;
+  const enteredContent = event.currentTarget.querySelector('#content').value;
+
+  createPost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    const postId = event.target.closest('li').id;
+    sendHttpRequest(
+      'DELETE',
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
