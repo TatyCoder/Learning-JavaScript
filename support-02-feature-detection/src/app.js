ef77743ac419cff7d29303b2eb61427524ce2f60
TatyCoder <tmilanes@gmail.com>
2020-12-14 17:02:08 -0500

    Support Exercises

---
+++ support-02-feature-detection/src/app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button');
+const textParagraph = document.querySelector('p');
+
+button.addEventListener('click', () => {
+  // To get the text:
+  const text = textParagraph.textContent;
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    alert('Feature not available, please copy manually!');
+  }
+});
