6b09b5f437bfe0c2ae58911d46683c2328891cb1
TatyCoder <tmilanes@gmail.com>
2021-01-27 15:28:46 -0500

    Node first node.js code

---
+++ node-01-first-nodejs-code/app.js
@@ -0,0 +1,6 @@
+const userName = 'Max';
+
+console.log(`Hi ${userName}!`);
+
+/* Notes: in Command Prompt I type cd followed by the folder I want, then I type node and the   
+file name I want to execute: node app.js. This will tell Node.js to run this Javascript file. */
