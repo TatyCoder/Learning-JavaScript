ec14c81d40764940da0330d4fcc419fe4cf5ed83
TatyCoder <tmilanes@gmail.com>
2021-02-02 16:35:06 -0500

    Node frontend basic post request setup

---
+++ node-frontend-01-basic-post-request-setup/src/SharePlace.js
@@ -0,0 +1,124 @@
+import { Modal } from './UI/Modal';
+import { Map } from './UI/Map';
+import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
+
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+    this.shareBtn = document.getElementById('share-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+    this.shareBtn.addEventListener('click', this.sharePlaceHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+  }
+
+  sharePlaceHandler() {
+    const sharedLinkInputElement = document.getElementById('share-link');
+    if (!navigator.clipboard) {
+      sharedLinkInputElement.select();
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(sharedLinkInputElement.value)
+      .then(() => {
+        alert('Copied into clipboard!');
+      })
+      .catch(err => {
+        console.log(err);
+        sharedLinkInputElement.select();
+      });
+  }
+
+  // To connect with the node-10-basic-rest-routes file (which is the backend):
+  selectPlace(coordinates, address) {
+    if (this.map) {
+      this.map.render(coordinates);
+    } else {
+      this.map = new Map(coordinates);
+    }
+    fetch('http://localhost:3000/add-location', { // Because this frontend has port 8080.
+      method: 'POST',  // Because it's a post request.
+      // Providing all the keys I'm looking for:
+      body: JSON.stringify({
+        address: address,
+        lat: coordinates.lat,
+        lng: coordinates.lng
+      }),
+      // To tell the backend that I'm sending some JSON data:
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    // To parse and return that JSON data:  
+    .then(response => {
+        return response.json();
+      })
+      // To get the data:
+      .then(data => {
+        console.log(data);
+      });
+    this.shareBtn.disabled = false;
+    const sharedLinkInputElement = document.getElementById('share-link');
+    sharedLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
+      coordinates.lng
+    }`;
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
+      );
+      return;
+    }
+    const modal = new Modal(
+      'loading-modal-content',
+      'Loading location - please wait!'
+    );
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      async successResult => {
+        const coordinates = {
+          lat: successResult.coords.latitude + Math.random() * 50,
+          lng: successResult.coords.longitude + Math.random() * 50
+        };
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
+      },
+      error => {
+        modal.hide();
+        alert(
+          'Could not locate you unfortunately. Please enter an address manually!'
+        );
+      }
+    );
+  }
+
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector('input').value;
+    if (!address || address.trim().length === 0) {
+      alert('Invalid address entered - please try again!');
+      return;
+    }
+    const modal = new Modal(
+      'loading-modal-content',
+      'Loading location - please wait!'
+    );
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
+    }
+    modal.hide();
+  }
+}
+
+const placeFinder = new PlaceFinder();
